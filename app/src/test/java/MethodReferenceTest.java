c5bdc5ca04c501bc4054232a2b7bfb571056bf92
mikael.baek <mikael.baek@daumkakao.com>
2016-05-14 16:24:05 +0900

    add SAM(Single Abstract Method - Functional Inteface) test

---
+++ app/src/test/java/MethodReferenceTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class MethodReferenceTest {
+    interface TestListener {
+        void onClick();
+    }
+
+    private void doIt(TestListener listener) {
+        listener.onClick();
+    }
+
+    @Test
+    public void pass_methodReference_for_sam() throws Exception {
+        doIt(this::method_instead_of_interface_impl);
+        // works if interface is SAM(functional interface)
+    }
+
+    private void method_instead_of_interface_impl() {
+        System.out.println("works");
+    }
+}
